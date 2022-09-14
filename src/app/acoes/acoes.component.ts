9ba4404320a1dcf4a00c885960ff222b101539b4
Rafael Neves <rnoliveira@band.com.br>
2022-09-14 02:04:49 -0300

    add

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -2,24 +2,26 @@ import { Acoes, AcoesAPI } from './model/acoes';
 import { AcoesService } from './acoes.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+import { merge, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit {
+export class AcoesComponent{
   acoesInput = new FormControl();
-  public acoes$ = this.acoesInput.valueChanges.pipe(
-    tap(console.log),
+  todasAcoes$ = this.acoesService.getAcoes();
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
     debounceTime(300),
-    filter((valorDigitado) => valorDigitado.length >= 2),
+    filter((valorDigitado) => valorDigitado.length >= 2 || !valorDigitado.length),
+    distinctUntilChanged(),
     switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
-    filter(Boolean),
-    tap(console.log)
   );
+
+  //combina metodo de duas formas
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
   constructor(private acoesService: AcoesService) {}
 
   ngOnInit() {}
